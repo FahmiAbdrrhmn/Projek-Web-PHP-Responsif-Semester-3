8c7a74538dd4bd568a6a91d5f2269d9547f9ea1f
Fahmi Abdurrahman <146101835+FahmiAbdrrhmn@users.noreply.github.com>
2024-12-29 14:48:26 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,40 +1,46 @@
-<!-- footer berisi kontak -->
+<!-- Footer Berisi Kontak -->
 <div class="container-fluid warna1 py-5 text-light">
     <div class="container">
         <h5 class="text-center mb-4">Cek Kami di</h5>
-        <div class="row justify-content-center">
-            <div class="col-3 col-md-2 col-lg-1 d-flex justify-content-center mb-3">
-                <a href="https://www.facebook.com/share/1XfR7fNSe1/" target="_blank">
-                    <i class="fab fa-facebook fs-4 text-light"></i>
+        <div class="row justify-content-center gy-3 gx-5">
+            <!-- Facebook -->
+            <div class="col-4 col-sm-3 col-md-2 col-lg-1 d-flex justify-content-center">
+                <a href="https://www.facebook.com/share/1XfR7fNSe1/" target="_blank" class="text-decoration-none">
+                    <i class="fab fa-facebook text-light fs-4"></i>
                 </a>
             </div>
-            <div class="col-3 col-md-2 col-lg-1 d-flex justify-content-center mb-3">
-                <a href="https://www.instagram.com/usualweirdguy/profilecard/?igsh=MTZtemtqc3lwcmUwcQ==" target="_blank">
-                    <i class="fab fa-instagram fs-4 text-light"></i>
+            <!-- Instagram -->
+            <div class="col-4 col-sm-3 col-md-2 col-lg-1 d-flex justify-content-center">
+                <a href="https://www.instagram.com/usualweirdguy/profilecard/?igsh=MTZtemtqc3lwcmUwcQ==" target="_blank" class="text-decoration-none">
+                    <i class="fab fa-instagram text-light fs-4"></i>
                 </a>
             </div>
-            <div class="col-3 col-md-2 col-lg-1 d-flex justify-content-center mb-3">
-                <a href="https://github.com/FahmiAbdrrhmn/Projek-Web-PHP-Responsif-Semester-3" target="_blank">
-                    <i class="fab fa-github fs-4 text-light"></i>
+            <!-- GitHub -->
+            <div class="col-4 col-sm-3 col-md-2 col-lg-1 d-flex justify-content-center">
+                <a href="https://github.com/FahmiAbdrrhmn/Projek-Web-PHP-Responsif-Semester-3" target="_blank" class="text-decoration-none">
+                    <i class="fab fa-github text-light fs-4"></i>
                 </a>
             </div>
-            <div class="col-3 col-md-2 col-lg-1 d-flex justify-content-center mb-3">
-                <a href="https://youtube.com/@fahmiabdurrahman318?si=gotuxxEygV-90HP_" target="_blank">
-                    <i class="fab fa-youtube fs-4 text-light"></i>
+            <!-- YouTube -->
+            <div class="col-4 col-sm-3 col-md-2 col-lg-1 d-flex justify-content-center">
+                <a href="https://youtube.com/@fahmiabdurrahman318?si=gotuxxEygV-90HP_" target="_blank" class="text-decoration-none">
+                    <i class="fab fa-youtube text-light fs-4"></i>
                 </a>
             </div>
-            <div class="col-3 col-md-2 col-lg-1 d-flex justify-content-center mb-3">
-                <a href="https://wa.me/qr/WECNP34T3FX4I1" target="_blank">
-                    <i class="fab fa-whatsapp fs-4 text-light"></i>
+            <!-- WhatsApp -->
+            <div class="col-4 col-sm-3 col-md-2 col-lg-1 d-flex justify-content-center">
+                <a href="https://wa.me/qr/WECNP34T3FX4I1" target="_blank" class="text-decoration-none">
+                    <i class="fab fa-whatsapp text-light fs-4"></i>
                 </a>
             </div>
         </div>
     </div>
 </div>
 
+<!-- Footer Copyright -->
 <div class="container-fluid warna1 text-light text-center py-3">
     <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
-        <span>&copy; 2024 Rizky Barokah</span>
-        <span>Pulled from Fahmi's Braincell</span>
+        <span class="my-1">&copy; 2024 Rizky Barokah</span>
+        <span class="my-1">Pulled from Fahmi's Braincell</span>
     </div>
 </div>
